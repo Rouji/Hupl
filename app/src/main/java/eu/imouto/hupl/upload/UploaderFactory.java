2f90ca320b5cbb7cc2a7fb0c56578f12535e2b7f
Andreas Hackl <a@r0.at>
2025-02-21 01:06:54 +0100

    use StringMapPreference thing for headers setting

--- app/src/main/java/eu/imouto/hupl/upload/UploaderFactory.java
+++ app/src/main/java/eu/imouto/hupl/upload/UploaderFactory.java
@@ -6,7 +6,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.Iterator;
@@ -35,8 +34,7 @@ class UploaderFactory
             up.authUser = getStr(entry.json, "authUser");
             up.authPass = getStr(entry.json, "authPass");
             up.disableChunkedTransfer = getBool(entry.json, "disableChunkedTransfer", false);
-            up.extraParams = getMap(entry.json, "extraParams");
-            up.headers = getHeaders(entry.json, "headers");
+            up.headers = getMap(entry.json, "headers");
             up.extraParams = getMap(entry.json, "extraParams");
             return up;
         }
@@ -82,29 +80,5 @@ class UploaderFactory
         catch (JSONException ex)
         {}
         return str;
-    }
-
-    private static Map<String, String> getHeaders(JSONObject obj, String name)
-    {
-        Map<String, String> headers = new HashMap<>();
-        String headersString = getStr(obj, "headers");
-        if (headersString == null || headersString.isBlank()) {
-            return headers;
-        }
-
-
-        String[] lines = headersString.split(Pattern.quote("\n"));
-        for (String line : lines) {
-            String[] lineSplit = line.split(Pattern.quote(":"));
-            if (lineSplit.length != 2) {
-                continue;
-            }
-
-            String key = lineSplit[0].trim();
-            String value = lineSplit[1].trim();
-            headers.put(key, value);
-        }
-
-        return headers;
     }
 }
